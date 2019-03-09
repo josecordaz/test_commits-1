41ce79b4c30369327533845cf544886e7c7b64ef
JOSE CARLOS ORDAZ CRIZANTOS <josecordaz@gmail.com>
2019-03-09 13:42:46 -0800

    Third commit directly on on master

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ console.log("Second Commit");
 	console.log("First commit for PR");
 	console.log("Second commit for PR");
 	console.log("Third commit for PR");
+console.log("Third commit on master"):
